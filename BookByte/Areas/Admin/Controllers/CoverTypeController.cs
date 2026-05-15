2a5a59ae0dbc4cdb5443d17f9a6b079ae716e213
happy <happy@HAPPYSONI>
2026-05-15 21:51:37 +0530

    test mfg

--- BookByte/Areas/Admin/Controllers/CoverTypeController.cs
+++ BookByte/Areas/Admin/Controllers/CoverTypeController.cs
@@ -9,7 +9,7 @@ namespace BookByte.Areas.Admin.Controllers
         private readonly IUnitOfWork _unitOfWork;   // ✅ private not public
         public CoverTypeController(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork;//test
         }
 
         public IActionResult Index()
